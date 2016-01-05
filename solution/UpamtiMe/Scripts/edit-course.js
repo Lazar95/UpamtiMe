c28f317c369d2d0915db1a37ed69322800eb1626
Lazar Ljubenović <lazar.ljubenovic.1995@gmail.com>
2016-01-05 12:22:42 +0100

    Dodavanje nivoa Ne radi na dugme. Jos.

---
+++ solution/UpamtiMe/Scripts/edit-course.js
@@ -0,0 +1,53 @@
+var _course = {
+
+}
+
+/*
+var viewToData = function() {
+  var course = $('.course');
+  var listOfLevels;
+  $.each(course, function())
+}
+*/
+
+var _dataToSend = {
+  "deletedCards":  [], // lista ID-jeva kartica
+  "deletedLevels": [], // lista Id-jeva nivoa
+  "editedCards":   [], // niz objekata kartica
+  "editedLevels":  [], // niz objekata nivoa
+}
+
+var addLevel = function() {
+  var newLevel = {
+    "id": -1,
+    "type": 0,
+    "name": "",
+  }
+
+  newLevel.type = $('#new-level select option:selected').val();
+  newLevel.name = $('#new-level input').val();
+
+  _dataToSend.editedLevels.push(newLevel);
+
+  dump();
+}
+
+/**
+ * Bindovanje funkcija za dugmad
+ */
+$('#new-level .add-button').click(function() {
+  alert('dafgsd');
+  //addLevel();
+});
+
+$( "#new-level .add-button" ).click(function() {
+  alert( "Handler for .click() called." );
+});
+
+var dump = function() {
+  console.log(_dataToSend);
+}
+
+$(document).ready(function() {
+
+});

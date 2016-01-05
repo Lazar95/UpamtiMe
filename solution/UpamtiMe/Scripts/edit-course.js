f48b7e70038c011dcc34ba575ed3153d9531cc32
Lazar Ljubenović <lazar.ljubenovic.1995@gmail.com>
2016-01-05 22:10:54 +0100

    Edit Course - pocetak dizajna i dodavnje
    Dizajn zapocet.
    Dodavanje novog levela i kartice spremno da se testira.

--- solution/UpamtiMe/Scripts/edit-course.js
+++ solution/UpamtiMe/Scripts/edit-course.js
@@ -1,14 +1,45 @@
-var _course = {
+var _course = []; // niz nivoa
 
-}
+var UNTOUCHED = 0;
+var CHANGED = 1;
+var NEW = 2;
 
-/*
 var viewToData = function() {
-  var course = $('.course');
-  var listOfLevels;
-  $.each(course, function())
+
+  $('#course > li:not(#new-level) > ul').each(function() {
+
+    var cardsInCurrLevel = [];
+
+    $(this).children('li:not(.new-card)').each(function() {
+      cardsInCurrLevel.push(
+        {
+          "cardID": $(this).attr('data-card-id'),
+          "number": $(this).attr('data-card-number'),
+          "question": $(this).children('.question').text(),
+          "answer": $(this).children('.answer').text(),
+          "status": UNTOUCHED,
+        }
+      );
+    }); // unutrasnji each
+
+    _course.push(
+      {
+        "levelID": $(this).attr('data-level-id'),
+        "name": $(this).children('span').html(),
+        "number": $(this).attr('data-level-number'),
+        "type": $(this).attr('data-level-type'),
+        "cards": cardsInCurrLevel,
+        "status": UNTOUCHED,
+      }
+    )
+
+  }); // spoljni each
+
 }
-*/
+
+
+var _lastFakeCardID = 0;
+var _lastFakeLevelID = 0;
 
 var _dataToSend = {
   "courseID": 0,
@@ -17,28 +48,103 @@ var _dataToSend = {
   "subcategoryID": 0,
   "deletedCards":  [], // lista ID-jeva kartica
   "deletedLevels": [], // lista Id-jeva nivoa
-  "editedCards":   [], // niz objekata kartica
-  "editedLevels":  [], // niz objekata nivoa
+  "editedCards":   [], // niz objekata kartica (promenjene a postojale)
+  "editedLevels":  [], // niz objekata nivoa (promenjen naziv ili redosled)
+  "addedCards":    [], // nove kartice
+  "addedLevels":   [], // nivo nivoi -- lazni ID (negativan)
 }
 
+/*****************************
+ * Dodavanje nivoa i kartica *
+ *****************************/
+
 var addLevel = function() {
+
+  _lastFakeLevelID--;
+
   var newLevel = {
-    "levelID": -1,
-    "type": 0,
-    "name": "",
+    "levelID": _lastFakeLevelID.toString(),
+    "number": (_course.length + 1).toString(),
+    "type": $('#new-level select option:selected').val(),
+    "name": $('#new-level input').val().toString(),
+    "cards": [],
+    "status": NEW,
   }
 
-  newLevel.type = $('#new-level select option:selected').val();
-  newLevel.name = $('#new-level input').val();
+  _course.push(newLevel);
 
-  _dataToSend.editedLevels.push(newLevel);
+  var string = '';
+  string += '<li>';
+    string += '<span>' + newLevel.name + '</span>';
+    string += '<ul class="level" data-level-id="' + newLevel.levelID + '" data-level-number="' + newLevel.number + '" data-level-type="' + newLevel.type + '">';
+      string += '<li class="new-card">';
+        string += 'Pitanje: <input class="question" type="text" />';
+				string += 'Odgovor: <input class="answer" type="text" />';
+				string += '<div class="add-button">Dodaj!</div>';
+      string += '</li>';
+    string += '</ul>';
+  string += '</li>';
+
+  $('#course > li:last-of-type').before(string);
+
+}
+
+var addCard = function(level) {
+
+  _lastFakeCardID--;
+
+  var newCard = {
+    "cardID": _lastFakeCardID.toString(),
+    "number": (_course[level - 1].cards.length + 1).toString(),
+    "question": $('#course > li:nth-child(' + level + ') .new-card input.question').val(),
+    "answer": $('#course > li:nth-child(' + level + ') .new-card input.answer').val(),
+    "level": _course[level - 1].levelID.toString(),
+    "status": NEW,
+  }
+
+  _course[level-1].cards.push(newCard);
+
+  var string = '';
+  string += '<li data-card-id="' + newCard.cardID + '" data-card-number="' + newCard.number + '">';
+    string += '<span class="question">' + newCard.question + '</span>';
+    string += '<span class="answer">' + newCard.answer + '</span>';
+    string += '<div class="change-button">Promeni</div>';
+  string += '</li>';
+
+  $('#course > li:nth-of-type(' + level + ') ul > li:last-of-type').before(string);
+
+}
+
+var save = function() {
+
+  // Osnovni podaci o kursu:
   _dataToSend.courseID = $('#course').attr('data-course-id');
-  _dataToSend.name = $('.course-name').val();
-  _dataToSend.categoryID = $('.course-cat').val();
-  _dataToSend.subcategoryID = $('.course-subcat').val();
+  _dataToSend.name = $('#course-name > span').html();
+  _dataToSend.categoryID = $('.cat-subcat #category option:selected').val();
+  _dataToSend.subcategoryID = $('.cat-subcat #subcategory option:selected').val();
 
-  dump();
+  // Dodati celi nivoi:
+  for (var i = 0; i < _course.length; i++) {
+    if (_course[i].status == NEW) {
+      _dataToSend.addedLevels.push(_course[i]);
+    }
+  }
 
+  // Dodate pojedinacne kartice:
+  for (var i = 0; i < _course.length; i++) {
+    if (_course[i].status != NEW) {
+      // Sve nove kartice iz nivoa koji nisu novi (nedirani ili im je promenjeno ime)
+      for (var j = 0; j < _course[i].cards.length; j++) {
+        if (_course[i].cards[j].status == NEW) {
+          _dataToSend.addedCards.push(_course[i].cards[j]);
+        }
+      }
+    }
+  }
+
+  console.log(_dataToSend)
+
+  /*
   $.ajax({
       url: "/Courses/EditCourse", // /kontroler/akcija (klasa/funkcija u klasi)
       method: "POST",
@@ -51,20 +157,24 @@ var addLevel = function() {
           }
       }
   });
-
+  */
 }
 
 /**
  * Bindovanje funkcija za dugmad
  */
-$('#new-level .add-button').click(function() {
-  addLevel();
+$('#course').on('click', '#new-level .add-button', addLevel);
+$('#course').on('click', '.new-card .add-button', function() {
+  var index = $(this).parent().parent().parent().index();
+  addCard(index+1);
 });
+$('#save').click(save);
+
 
 var dump = function() {
   console.log(_dataToSend);
 }
 
 $(document).ready(function() {
-
+  viewToData();
 });

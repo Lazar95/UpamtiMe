e736274b4b52e4436dd61e530ee8bc405cd20706
Lazar Ljubenović <lazar.ljubenovic.1995@gmail.com>
2016-01-25 09:43:31 +0100

    Create Course validacija
    Ne moze da se posalje zahtev ako nisu uneta sva polja.

--- solution/UpamtiMe/Scripts/create-new-course.js
+++ solution/UpamtiMe/Scripts/create-new-course.js
@@ -7,3 +7,19 @@ var hideCategories = function(cat, subcat) {
 $('#new-course-category > select').change(function() {
   hideCategories($(this), $('#new-course-subcategory > select'));
 });
+
+$('#create-new-course-button').click(function() {
+  var name = $('#new-course-name').val();
+  var cat = $('#new-course-category > select').children(':selected').val();
+  var subcat = $('#new-course-subcategory > select').children(':selected').val();
+  var error = "";
+  if (name == "" || name == undefined)                      error += 'Kurs mora imati ime! ';
+  if (cat == "0" || cat == "" || cat === undefined)         error += 'Kurs mora pripadati nekoj kategoriji! ';
+  if (subcat == "0" || subcat == "" || subcat == undefined) error += 'Kurs mora pripadati nekoj podkategoriji! ';
+  if (error == "") {
+    return true;
+  } else {
+    alert(error);
+    return false;
+  }
+});

0fc4582240f546ea214508a8b5650c596e20daec
Lazar Ljubenović <lazar.ljubenovic.1995@gmail.com>
2016-01-30 16:36:55 +0100

    CourseProfile: Spisak WIP

--- solution/UpamtiMe/Scripts/course-profile.js
+++ solution/UpamtiMe/Scripts/course-profile.js
@@ -43,5 +43,10 @@ $('#leaderboard-tabs').on('click', 'li', function() {
   }
 });
 
+// Spsiak
+$('.levels').on('click', '.button-level-linky', function() {
+  $('#list-of-cards').addClass('visible');
+});
+
 $(document).ready(function() {
 });

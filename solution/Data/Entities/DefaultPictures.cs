ecbbc757eb83280c3bc3dd31ad4b37a0eca24494
Plavusa <masadordevic@gmail.com>
2016-02-03 09:05:30 +0100

    brisanje defaultnih slika fix

--- solution/Data/Entities/DefaultPictures.cs
+++ solution/Data/Entities/DefaultPictures.cs
@@ -25,7 +25,39 @@ namespace Data
         public static void removeImage(int imgID)
         {
             DataClasses1DataContext dc = new DataClasses1DataContext();
+           
             DefaultPicture dp = (from a in dc.DefaultPictures where a.defaultPictureID == imgID select a).First();
+            //svima koji su imali tu defaultnu sliku svlja neku drugu
+            var newDefaultPicture =
+                (from a in dc.DefaultPictures where a.defaultPictureID != imgID && a.type == dp.type select a.defaultPictureID);
+            int newDefaultPictureID;
+            if (newDefaultPicture.Any())
+            {
+                newDefaultPictureID = newDefaultPicture.First();
+            }
+            else
+            {
+                return;//ne moze da obrise tu defaultnu ako ne postoji druga koja ce da je zameni
+            }
+
+            if (dp.type == (int)Enumerations.DefaultPicture.Avatar)
+            {
+                List<User> users = (from a in dc.Users select a).ToList();
+                foreach (User user in users)
+                {
+                    if (user.defaultAvatarID == imgID)
+                        user.defaultAvatarID = newDefaultPictureID;
+                }
+            }
+            else if (dp.type == (int) Enumerations.DefaultPicture.Course)
+            {
+                List<Course> courses = (from a in dc.Courses select a).ToList();
+                foreach (Course course in courses)
+                {
+                    if (course.defaultImageID == imgID)
+                        course.defaultImageID = newDefaultPictureID;
+                }
+            }
             dc.DefaultPictures.DeleteOnSubmit(dp);
             dc.SubmitChanges();
         }
